5b6c8bda12b907a1df1ee27ee0bb4bd6cfc959f4
green <green@orlov.com.ua>
2017-06-30 00:22:44 +0300

    header, js for top-nav

--- js/js.js
+++ js/js.js
@@ -1,23 +1,6 @@
-function resizeSquare () {
-    var $square = $('#square');
-    var width = $square.width();
-    console.log( width );
-    $square.css("height", width + "px");
-}
-
 $(document).ready( function () {
-    resizeSquare();
-    $(window).on('resize', resizeSquare );
-});
-
-$(document).ready( function () {
-    $(document).ready(function() {
-        slider = $('.slider-big ul').lightSlider({
-            item:1,
-            loop:true,
-            slideMove:1,
-            easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-            speed:600
-        });
+    $(".nav-button").click(function() {
+        $($('nav').toggle('slow'));
+        return false;
     });
 });
