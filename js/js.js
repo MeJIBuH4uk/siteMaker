72692d229d2489571fcbdc0643d3ef89fcb64aa5
green <green@orlov.com.ua>
2017-06-30 22:36:08 +0300

    js hide/show for content section

--- js/js.js
+++ js/js.js
@@ -4,3 +4,19 @@ $(document).ready( function () {
         return false;
     });
 });
+
+$('.basics .title').click(function () {
+    $($('.basics .hr, .basics p, .basics ul').toggle('slow'));
+    $($('.basics .title').css("margin-bottom","0"));
+    return false;
+});
+$('.tools .title').click(function () {
+    $($('.tools .hr, .tools p, .tools ul').toggle('slow'));
+    $($('.tools .title').css("margin-bottom","0"));
+    return false;
+});
+$('.programms .title').click(function () {
+    $($('.programms .hr, .programms p, .programms ul').toggle('slow'));
+    $($('.programms .title').css("margin-bottom","0"));
+    return false;
+});
