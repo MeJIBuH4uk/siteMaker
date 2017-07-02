5e7de97832c2f0140a0a7a5fefce8a3602a3c959
green <green@orlov.com.ua>
2017-07-02 22:36:09 +0300

    slider, and buttons progs

--- js/js.js
+++ js/js.js
@@ -3,20 +3,32 @@ $(document).ready( function () {
         $($('nav').toggle('slow'));
         return false;
     });
+	
+	
+	$('.basics .title').click(function () {
+		$($('.basics .hr, .basics p, .basics ul').toggle('slow'));
+		$($('.basics li p').css("display","inline"));
+		$($('.basics .title').css("margin-bottom","0"));
+		return false;
+	});
+	
+	$('.tools .title').click(function () {
+		$($('.tools .hr, .tools p, .tools ul').toggle('slow'));
+		$($('.tools li p').css("display","inline"));
+		$($('.tools .title').css("margin-bottom","0"));
+		return false;
+	});
+	
+	$('.programms .title').click(function () {
+		$($('.programms .hr, .programms p, .programms ul').toggle('slow'));
+		$($('.programms li p').css("display","inline"));
+		$($('.programms .title').css("margin-bottom","0"));
+		return false;
+	});
 });
 
-$('.basics .title').click(function () {
-    $($('.basics .hr, .basics p, .basics ul').toggle('slow'));
-    $($('.basics .title').css("margin-bottom","0"));
-    return false;
-});
-$('.tools .title').click(function () {
-    $($('.tools .hr, .tools p, .tools ul').toggle('slow'));
-    $($('.tools .title').css("margin-bottom","0"));
-    return false;
-});
-$('.programms .title').click(function () {
-    $($('.programms .hr, .programms p, .programms ul').toggle('slow'));
-    $($('.programms .title').css("margin-bottom","0"));
-    return false;
+$('.slider ul').bxSlider({
+	mode: 'fade',
+	captions: true,
+	controls: true
 });
